8538e13c0a7ec7e56590a49e4d8b701e4e61d8b6
Nevena <nevena.milojevic@dev.telnet.rs>
2024-08-26 16:35:45 +0200

    todo list

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("ToDo Application");
+const userName = document.getElementById("user_name");
+const userMessage = document.getElementById("user_message");
+
+// function add(e) {
+//     let text = res.value;
+//     document.getElementById("results").innerHTML = text;
+//   console.log(e);
+// }
+
+document.getElementById("button").addEventListener("click", function (e) {
+  e.preventDefault();
+  let userNameText = userName.value;
+  let userMessageText = userMessage.value;
+
+  document.getElementById("name_results").innerHTML += userNameText;
+  userName.value = "";
+  document.getElementById("message_results").innerHTML += userMessageText;
+  userMessage.value = "";
+});
