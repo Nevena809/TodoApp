bf61f2efa6a0a03fa4f0d5f83250220b718c912f
Nevena <nevena.milojevic@dev.telnet.rs>
2024-08-29 15:40:59 +0200

    delete is working, edit is not working

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let todos = JSON.parse(localStorage.getItem("todos")) || [];
 const todoName = document.getElementById("todo_name");
 const addButton = document.getElementById("add_button");
 const nameResults = document.getElementById("name_results");
-const deleteButton = document.getElementById("delete_button");
+const deleteAllButton = document.getElementById("delete_all_button");
 const messageResults = document.getElementById("message_results");
+const increasingButton = document.getElementById("increasing_button");
+const decreasingButton = document.getElementById("decreasing_button");
+
+let orderInc = "asc";
 
 document.addEventListener("DOMContentLoaded", function () {
   displayTasks();
@@ -15,7 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
       addTaskName();
     }
   });
-  deleteButton.addEventListener("click", deleteAllTasks);
+  deleteAllButton.addEventListener("click", deleteAllTasks);
+  increasingButton.addEventListener("click", increasingSortTask);
+  decreasingButton.addEventListener("click", decreasingSortTask);
 });
 
 function addTaskName() {
@@ -25,9 +31,10 @@ function addTaskName() {
       name: newTaskName,
       disabled: false,
     });
-    saveToLocalStorage();
+
+    sortTasks();
+
     todoName.value = "";
-    displayTasks();
   }
 }
 
@@ -37,10 +44,11 @@ function displayTasks() {
     const p = document.createElement("p");
     p.innerHTML = `
       <div class="todo-container">
-      <input type="checkbox" class="todo-checkbox" 
-      id="input_${index}" ${item.disabled ? "checked" : ""}>
-      <p id="${index}" class="${item.disabled ? "disabled" : ""} 
-      onClick="editTask(${index})"> ${item.name} </p>
+        <input type="checkbox" class="todo-checkbox" 
+        id="input_${index}" ${item.disabled ? "checked" : ""}>
+        <p id="todo-${index}" class="todo-p ${item.disabled ? "disabled" : ""} 
+        onClick="editTask(${index})"> ${item.name} </p>
+        <button id="${index}" class="delete-task" onclick="deleteTask(${index})">Delete</button>
       </div>
     `;
     p.querySelector(".todo-checkbox").addEventListener("change", () =>
@@ -49,6 +57,25 @@ function displayTasks() {
     nameResults.appendChild(p);
   });
 }
+
+// function editTask(index) {
+//   const todoTask = documen.getElementById(`todo-${index}`);
+//   const exsitingText = todos[index].name;
+//   const editOldTask = document.createElement("input");
+
+//   editOldTask.value = exsitingText;
+//   todoTask.replaceWith(editOldTask);
+//   editOldTask.focus();
+
+//   editOldTask.addEventListener("blur", function () {
+//     const updateText = editOldTask.value.trim();
+//     if (updateText) {
+//       todos[index].name = updateText;
+//       saveToLocalStorage();
+//     }
+//     displayTasks();
+//   });
+// }
 
 function toogleTask(index) {
   todos[index].disabled = !todos[index].disabled;
@@ -64,4 +91,32 @@ function deleteAllTasks() {
   todos = [];
   saveToLocalStorage();
   displayTasks();
+}
+
+function deleteTask(index) {
+  todos.splice(index, 1);
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function increasingSortTask() {
+  orderInc = "asc";
+  todos.sort((a, b) => a.name.localeCompare(b.name));
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function decreasingSortTask() {
+  orderInc = "desc";
+  todos.sort((a, b) => b.name.localeCompare(a.name));
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function sortTasks() {
+  if (orderInc === "asc") {
+    increasingSortTask();
+  } else {
+    decreasingSortTask();
+  }
 }
